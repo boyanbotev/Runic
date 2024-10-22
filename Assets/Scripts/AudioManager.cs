ea6912f2761f1f0fbc103c0815464a653879b2cd
Boyan Botev <boyanbotev@gmail.com>
2024-10-22 19:51:30 +0300

    fix: add sounds

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private Sound[] sounds;
+    private static GameObject oneShotGameObject;
+    private static AudioSource oneShotAudioSource;
+
+    private void OnEnable()
+    {
+        DraggableLetter.onSelect += PlayLetterSound;
+    }
+
+    private void OnDisable()
+    {
+        DraggableLetter.onSelect -= PlayLetterSound;
+    }
+
+    void PlayLetterSound(DraggableLetter letter)
+    {
+        PlaySound(letter.value);
+    }
+
+    void PlayIncorrectSound()
+    {
+        PlaySound("incorrect");
+    }
+
+
+    private void PlaySound(string sound)
+    {
+        if (oneShotAudioSource == null)
+        {
+            oneShotGameObject = new GameObject("One shot sound");
+            oneShotAudioSource = oneShotGameObject.AddComponent<AudioSource>();
+        }
+
+        oneShotAudioSource.PlayOneShot(GetAudioClip(sound));
+    }
+
+    private AudioClip GetAudioClip(string sound)
+    {
+        foreach (Sound soundRef in sounds)
+        {
+            if (soundRef.name == sound)
+            {
+                return soundRef.audioClip;
+            }
+        }
+        Debug.LogError("Sound " + sound + " not found");
+        return null;
+    }
+}
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip audioClip;
+}
